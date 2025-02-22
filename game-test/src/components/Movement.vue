35da0e42b18007bcce33b89171551621d0eb7f06
AmauryGuillin <amauryg@hotmail.fr>
2025-02-22 13:24:18 +0100

    first commit

---
+++ game-test/src/components/Movement.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const positionX = ref(900);
+const positionY = ref(350);
+
+function moveDot(input: KeyboardEvent) {
+  if (input.key === "ArrowUp") {
+    positionY.value -= 20;
+  } else if (input.key === "ArrowDown") {
+    positionY.value += 20;
+  } else if (input.key === "ArrowLeft") {
+    positionX.value -= 20;
+  } else if (input.key === "ArrowRight") {
+    positionX.value += 20;
+  }
+
+  if (positionX.value >= window.innerWidth - 100) {
+    console.log("hors limites X droite");
+    router.push("/map3");
+  }
+
+  if (positionX.value < 0) {
+    console.log("hors limites X gauche");
+    router.push("/map2");
+  }
+
+  if (positionY.value >= window.innerHeight - 100) {
+    console.log(window.innerHeight);
+    console.log("hors limites Y inférieure");
+  }
+
+  if (positionY.value < 0) {
+    console.log("hors limites Y supérieure");
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("keydown", moveDot);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", moveDot);
+});
+</script>
+
+<template>
+  <section
+    class="h-screen max-h-screen w-full max-w-full overflow-x-hidden bg-gray-900 relative"
+  >
+    <div class="text-white">Movement</div>
+    <div
+      id="character"
+      class="h-40 w-40 absolute"
+      :style="{ left: `${positionX}px`, top: `${positionY}px` }"
+    >
+      <img src="/character.gif" alt="player image" class="w-full" />
+    </div>
+  </section>
+</template>
---
+++ game-test/src/router.ts
@@ -0,0 +1,17 @@
+import { createMemoryHistory, createRouter } from "vue-router";
+import Map2 from "./components/Map2.vue";
+import Map3 from "./components/Map3.vue";
+import Movement from "./components/Movement.vue";
+
+const routes = [
+  { path: "/", component: Movement },
+  { path: "/map2", component: Map2 },
+  { path: "/map3", component: Map3 },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+export { router };
